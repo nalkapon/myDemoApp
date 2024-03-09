05d0b5c864fa572790af6ba38ee75ecb44092f78
nalkapon <y.dilaver@etu.edu.tr>
2024-03-09 03:23:53 +0300

    I have started my awesome projectgit statusgit status It will be great.

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class App {
+
+    public static void main(String[] args) {
+    		
+    }
+
+    public static int performOperation(ArrayList<Integer> list1, ArrayList<Integer> list2, int param1, int param2) {
+        int sum1 = 0;
+        for (int num : list1) {
+            sum1 += num;
+        }
+        int sum2 = 0;
+        for (int num : list2) {
+            sum2 += num;
+        }
+
+        int result = (sum1 + sum2) * (param1 - param2);     
+        return result;
+    }
+}
---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,89 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.*;
+
+public class AppTest {
+
+    public static void test1() {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(3);
+        list1.add(5);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(2);
+        list2.add(4);
+
+        int param1 = 10;
+        int param2 = 5;
+        int result = App.performOperation(list1, list2, param1, param2);
+        int expected = ((3 + 5) + (2 + 4)) * (10 - 5);
+        assertEquals(expected, result);
+    }
+
+    public static void test2() {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(-3);
+        list1.add(-5);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(-2);
+        list2.add(-4);
+        int param1 = -10;
+        int param2 = -5;
+
+        int result = App.performOperation(list1, list2, param1, param2);
+        int expected = ((-3 + -5) + (-2 + -4)) * (-10 - (-5));
+        assertEquals(expected, result);
+    }
+
+    public static void test3() {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(0);
+        list1.add(0);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(0);
+        list2.add(0);
+        int param1 = 0;
+        int param2 = 0;
+
+        int result = App.performOperation(list1, list2, param1, param2);
+        int expected = ((0 + 0) + (0 + 0)) * (0 - 0);
+        assertEquals(expected, result);
+    }
+
+    public static void test4() {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        int param1 = 10;
+        int param2 = 5;
+
+        int result = App.performOperation(list1, list2, param1, param2);
+        int expected = ((0)+(0)) * (10-5);
+        assertEquals(expected, result);
+    }
+
+    public static void test5() {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(10);
+        list1.add(15);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(20);
+        list2.add(40);
+
+        int param1 = 10;
+        int param2 = 5;
+
+        int result = App.performOperation(list1, list2, param1, param2);
+        int expected = ((3 + 5) + (2 + 4)) * (10 - 5);
+        assertEquals(expected, result);
+    }
+}
